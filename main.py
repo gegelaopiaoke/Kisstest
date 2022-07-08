ed88b21116bba4f7078572af760e4dd0f748b72b
Jerry <wanyihao@crazymaplestudio.com>
2022-07-08 17:07:56 +0800

    demo1： 初始化poco，屏蔽日志 启动app

---
+++ main.py
@@ -0,0 +1,14 @@
+import logging
+
+from airtest.core.api import start_app
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+
+poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
+# cmd里面使用adb shell pm list packages 查看包名
+# 或者使用代码os.system('adb shell pm list packages')查看
+# 记得替换adb文件，airtest自带的40版本的，我们默认41版本的，或者运行代码的时候别启动airtest的IDE
+# 以下屏蔽多余日志信息
+logger = logging.getLogger("airtest")
+logger.setLevel(logging.ERROR)
+start_app("com.stardust.kissreader")
+# start_app是启动,stop_app是停止app,clear_app是清除缓存
